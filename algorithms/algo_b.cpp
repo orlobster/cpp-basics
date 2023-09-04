92e14b682d3338d6ccb7e9de1c7b401a864f1e0d
freezzzingmoon <122732625+orlobster@users.noreply.github.com>
2023-09-05 01:39:14 +0300

    add algo_b

--- algorithms/algo_b.cpp
+++ algorithms/algo_b.cpp
@@ -1,6 +1,18 @@
+#include <algorithm>
+
 template <typename Iter>
 Iter Unique(Iter first, Iter last) {
-    while (first != last) {
-
+    auto iter1 = first;
+    auto iter2 = first;
+    while (iter2 != last) {
+        if (*iter1 != *iter2) {
+            *iter1 = std::move(*iter2);
+        }
+        ++iter1;
+        const auto& value = *iter2;
+        while (iter2 != last && *iter2 == value) {
+            ++iter2;
+        }
     }
+    return iter1;
 }
